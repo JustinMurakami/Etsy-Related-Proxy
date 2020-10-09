f181be4b9e6068aefe09dc8e42c9ea81066b3342
Justin Murakami <jmurakami84@gmail.com>
2020-10-09 15:37:57 -0700

    generalized to listen on every available network interface.

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 //Navbar endpoints
-app.use('/user', createProxyMiddleware({ target: 'http://localhost:8001', changeOrigin: true }));
-app.use('/entry', createProxyMiddleware({ target: 'http://localhost:8001', changeOrigin: true }));
+app.use('/user', createProxyMiddleware({ target: 'http://0.0.0.0:8001', changeOrigin: true }));
+app.use('/entry', createProxyMiddleware({ target: 'http://0.0.0.0:8001', changeOrigin: true }));
 //displayproduct
-app.use('/product-display', createProxyMiddleware({ target: 'http://localhost:8002', changeOrigin: true }));
-app.use('/photo-display', createProxyMiddleware({ target: 'http://localhost:8002', changeOrigin: true }));
+app.use('/product-display', createProxyMiddleware({ target: 'http://0.0.0.0:8002', changeOrigin: true }));
+app.use('/photo-display', createProxyMiddleware({ target: 'http://0.0.0.0:8002', changeOrigin: true }));
 //Reviews
-app.use('/reviewsItem', createProxyMiddleware({ target: 'http://localhost:8003', changeOrigin: true }));
-app.use('/reviewsShop', createProxyMiddleware({ target: 'http://localhost:8003', changeOrigin: true }));
+app.use('/reviewsItem', createProxyMiddleware({ target: 'http://0.0.0.0:8003', changeOrigin: true }));
+app.use('/reviewsShop', createProxyMiddleware({ target: 'http://0.0.0.0:8003', changeOrigin: true }));
 //Seller
-app.use('/api', createProxyMiddleware({ target: 'http://localhost:8004', changeOrigin: true }));
+app.use('/api', createProxyMiddleware({ target: 'http://0.0.0.0:8004', changeOrigin: true }));
 //Related
-app.use('/related', createProxyMiddleware({ target: 'http://localhost:8005', changeOrigin: true }));
+app.use('/related', createProxyMiddleware({ target: 'http://0.0.0.0:8005', changeOrigin: true }));
 app.listen(3000);
